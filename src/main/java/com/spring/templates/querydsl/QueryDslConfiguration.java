591cc31d4d9e54d27ccf890fc5bb369f8ba318d6
Konstantin <konstantin@revimedia.com>
2020-12-23 22:00:32 +0200

    configured query-sql maven plugin; schema pattern does not work

--- src/main/java/com/spring/templates/querydsl/QueryDslConfiguration.java
+++ src/main/java/com/spring/templates/querydsl/QueryDslConfiguration.java
@@ -1,19 +1,37 @@
 package com.spring.templates.querydsl;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.H2Templates;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
+import com.querydsl.sql.spring.SpringConnectionProvider;
+import com.querydsl.sql.spring.SpringExceptionTranslator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.inject.Provider;
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Configuration
 public class QueryDslConfiguration {
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager();
+    }
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
+    public com.querydsl.sql.Configuration querydslConfiguration() {
+        SQLTemplates templates = H2Templates.builder().build(); //change to your Templates
+        com.querydsl.sql.Configuration configuration = new com.querydsl.sql.Configuration(templates);
+        configuration.setExceptionTranslator(new SpringExceptionTranslator());
+        return configuration;
+    }
+
+    @Bean
+    public SQLQueryFactory queryFactory(DataSource dataSource) {
+        Provider<Connection> provider = new SpringConnectionProvider(dataSource);
+        return new SQLQueryFactory(querydslConfiguration(), provider);
     }
 }
